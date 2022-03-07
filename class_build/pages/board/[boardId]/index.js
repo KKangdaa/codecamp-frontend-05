1c860135378b734fd7095e6c887208fcbc1c002e
KKangda <kkangda712@gmail.com>
2022-03-07 12:28:40 +0900

    Update dynamic-routing

--- class_build/pages/board/[boardId]/index.js
+++ class_build/pages/board/[boardId]/index.js
@@ -1,11 +1,26 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function BoardDetailPage() {
   const router = useRouter();
 
   return (
     <div>
-      안녕하세요! 게시글 상세페이지 입니다. 게시글ID는 {router.query.boardId}
+      <Head>
+        <meta property="og:title" content="나의 게시판 입니다" />
+        <meta
+          property="og:description"
+          content="저의 게시판에 오신것을 환영합니다."
+        />
+        {/* <meta  property="og:image" content="/images/login-background.png" /> */}
+        <meta
+          property="og:image"
+          content="https://dullyshin.github.io/2018/08/30/HTML-imgLink/#lg=1&slide=0"
+        />
+      </Head>
+      <div>
+        안녕하세요! 게시글 상세페이지 입니다. 게시글ID는 {router.query.boardId}
+      </div>
     </div>
   );
 }
