83f76fafd49529c74aebb1803abbaaa78588cf4a
KKangda <kkangda712@gmail.com>
2022-03-07 11:21:26 +0900

    Update dynamic-routing

---
+++ class_build/pages/board/[boardId]/index.js
@@ -0,0 +1,10 @@
+import {useRouter} = from 'next/router'
+
+export default function BoardDetailPage() {
+
+  const router = useRouter()
+
+  return (
+    <div>안녕하세요! 게시글 상세페이지 입니다. 게시글ID는 {router.query.boardId}</div>
+  )
+}
